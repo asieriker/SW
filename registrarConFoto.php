5b19a561b30f39b265cf9a9b1ab612aa5950e0d6
Asier Tamayo <Asier Tamayo>
2016-10-09 12:40:27 +0200

    Añadida imagen en caso de no elegir ninguna

--- registrarConFoto.php
+++ registrarConFoto.php
@@ -9,6 +9,9 @@ echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
 $rutaEnServidor='imagenes';
 $rutaTemporal=$_FILES['foto']['tmp_name'];
 $nombreImagen=$_FILES['foto']['name'];
+if (empty($nombreImagen)) {
+	$nombreImagen='nodisponible.png';
+}
 $rutaDestino=$rutaEnServidor.'/'.$nombreImagen;
 move_uploaded_file($rutaTemporal, $rutaDestino);
 $sql="INSERT INTO Usuario (NombreApellidos, Correo, Contrasena, NTelefono, Especialidad, Intereses, ruta) VALUES ('$_POST[nombreyapellidos]','$_POST[direcciondecorreo]','$_POST[password]',$_POST[numerodetelefono],'$_POST[especialidad]','$_POST[intereses]', '$rutaDestino')";
